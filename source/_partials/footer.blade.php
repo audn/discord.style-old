9601acf0292e22839e0efb62fcbc28c26adfd53e
Audun <audunhilden@live.no>
2019-03-12 15:13:12 +0100

    added footer, not displayed yet

---
+++ source/_partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-black-darker text-white antialiased py-8 absolute pin-b">
+    <div class="max-w-lg mx-auto px-4 text-center">
+        <h2 class="text-3xl mb-2 font-normal">Discord Templates</h2>
+        <p class="block text-xs uppercase max-w-xs mx-auto leading-normal">This website is in no way affiliated with the program Discord.</p>
+    </div>
+</footer>
