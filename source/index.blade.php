1f7716eba443bfcc44984089e412833bd97ebf1f
Audun <audunhilden@live.no>
2019-03-12 12:12:51 +0100

    made index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,10 +1,17 @@
-
 @extends('_layouts.master')
 
 {{-- https://laravel.com/docs/5.4/blade#stacks --}}
 @push('head')
 
     {{-- You can use the head stack to push page specific elements to the head, such as stylesheets --}}
+    <noscript>
+        <style>
+            .lqip {
+                display: none !important;
+            }
+        </style>
+    </noscript>
+
     @endpush
 
     @section('title', ' Discord Templates')
@@ -12,153 +19,28 @@
     {{-- https://laravel.com/docs/5.4/blade#template-inheritance --}}
     @section('body')
 
-    <div class="container center-div">
-      <div class="row valign-wrapper no-valign-wrap-on-mobile">
-        <div class="col s12 l19">
-          <div class="discord-mockup z-depth-3">
-            <div class="items-panel">
-              <div class="actions">
-                <div class="close"></div>
-                <div class="minimize"></div>
-                <div class="zoom"></div>
+    <div class="container mx-auto leading-normal pt-80">
+        <div class="flex p-4 justify-center">
+            <div class="flex-1 max-w-lg mt-8">
+              <div class="mb-10">
+                <h1 class="text-3xl lg:text-4xl mb-6 text-white text-center -mb-px">Free Discord server templates!</h1>
+                <p class="text-grey-light text-center">Just click the button to get started.</p>
               </div>
-              <div class="items">
-                <div class="item"><a href="#"><img class="responsive-img" src="https://i.imgur.com/0z3EQVg.png"/></a></div>
-                <div class="item"><a href="https://apex-hub.com" target="_Blank"><img class="responsive-img" src="https://cdn.discordapp.com/icons/550004577404190729/04c1fac06656f28ad0ad5c3e8a2a6fea.png?"/></a></div>
-                <div class="item">
-                  <button class="guild-add">
-                    <span style="top: -4px;left: 2px;right: 0px; position: absolute;">
-                      +
-                    </span>
-                  </button>
+                <div class="text-center">
+                  <a href="/start" class="btn-blue text-white text-center no-underline ">
+                    Random template
+                  </a>
+                  <a href="#" class="btn-black text-white text-center no-underline ">
+                    Learn more
+                  </a>
                 </div>
-              </div>
             </div>
-            <div class="sidebar-panel hide-on-med-and-down">
-              <div class="header">Discord Templates</div>
-              <div class="items" id="items">
-                <div class="category">
-                  <path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 10L12 15 17 10"></path>
-                  Welcome to server
-                  <svg style="float:right;" name="PlusAlt" class="addButtonIcon-3u-3Hu" width="18" height="18" viewBox="0 0 18 18">
-                  <polygon fill-rule="nonzero" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8">
-                  </polygon>
-                </svg>
-               </div><a href="#">
-                  <div class="item trigger-group active" data="default">help</div></a><a href="#">
-                  <div class="item trigger-group" data="tips">tips</div></a><a href="#">
-                  <div class="item trigger-group locked" data="staff-chat-all">staff-chat-all</div></a>
-             </div>
-            </div>
-            <div class="messages-panel">
-              <div class="messages-group hide-unless-show" id="default">
-                <div class="header">help</div>
-                <div class="messages">
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://i.imgur.com/A256kGf.jpg"/></div>
-                    <div class="body">
-                      <div class="user-name">Adam</div>
-                      <div class="content">Hello</div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://i.imgur.com/624tugY.jpg"/></div>
-                    <div class="body">
-                      <div class="user-name">Eve</div>
-                      <div class="content">🍎 <i>*Eats apple*</i></div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1105210644406644736/lc7r2HOg_200x200.jpg"/></div>
-                    <div class="body">
-                      <div class="user-name">God</div>
-                      <div class="content">Are you fucking serious?
-                      </div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img ml-2 r180" style="fill:red;" src="https://cdn.discordapp.com/attachments/534174053184045057/554775908683350018/join.png"/></div>
-                    <div class="body">
-                      <div class="content"><strong>God</strong> <span style="color:hsla(0,0%,100%,.6)">left the server</span></div>
-                    </div>
-                  </div>
-
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
-                    <div class="body">
-                      <div class="content"><strong>Envy</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
-                    <div class="body">
-                      <div class="content"><strong>Greed</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
-                    <div class="body">
-                      <div class="content"><strong>Gluttony</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
-                    <div class="body">
-                      <div class="content"><strong>Lust</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
-                    <div class="body">
-                      <div class="content"><strong>Pride</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
-                    <div class="body">
-                      <div class="content"><strong>Sloth</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
-                    </div>
-                  </div>
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
-                    <div class="body">
-                      <div class="content"><strong>Wrath</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
-                    </div>
-                  </div>
-
-
-
-                </div>
-              </div>
-              <div class="messages-group hide-unless-show" id="tips">
-                <div class="header">tips</div>
-                <div class="messages">
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
-                    <div class="body">
-                      <div class="user-name">audn</div>
-                      <div class="content">You can click channels to rename them!</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="messages-group hide-unless-show" id="staff-chat-all">
-                <div class="header">staff-chat-all</div>
-                <div class="messages">
-                  <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
-                    <div class="body">
-                      <div class="user-name">audn</div>
-                      <div class="content">Hello dear staff, should we ban the cat from general?</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
+    @include('_partials.lazyload-image', [
+        'src' => 'https://mee6.xyz/assets/8760e4297fb4b5644eabd7fd3b16370a.png',
+        'class' => 'absolute pin-b bg-center block mx-auto'
+    ])
 
 
 @endsection
--- source/start.blade.php
+++ source/start.blade.php
@@ -24,19 +24,33 @@
               </div>
               <div class="items">
                 <div class="item"><a href="#"><img class="responsive-img" src="https://i.imgur.com/0z3EQVg.png"/></a></div>
-                <!--
+                <div class="item"><a href="https://apex-hub.com" target="_Blank"><img class="responsive-img" src="https://cdn.discordapp.com/icons/550004577404190729/04c1fac06656f28ad0ad5c3e8a2a6fea.png?"/></a></div>
                 <div class="item">
-                  <button class="guild-add">+</button>
+                  <a href="#">
+                    <button class="guild-add item trigger-group locked" data="add-server">
+                      <span style="top: -4px;left: 2px;right: 0px; position: absolute;">
+                        +
+                      </span>
+                    </button>
+                  </a>
                 </div>
-              -->
               </div>
             </div>
             <div class="sidebar-panel hide-on-med-and-down">
               <div class="header">Discord Templates</div>
-              <div class="items" id="items"><a href="#">
+              <div class="items" id="items">
+                <div class="category">
+                  <path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 10L12 15 17 10"></path>
+                  Welcome to server
+                  <svg style="float:right;" name="PlusAlt" class="addButtonIcon-3u-3Hu" width="18" height="18" viewBox="0 0 18 18">
+                  <polygon fill-rule="nonzero" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8">
+                  </polygon>
+                </svg>
+               </div><a href="#">
                   <div class="item trigger-group active" data="default">help</div></a><a href="#">
                   <div class="item trigger-group" data="tips">tips</div></a><a href="#">
                   <div class="item trigger-group locked" data="staff-chat-all">staff-chat-all</div></a>
+                  <div style="display:none" class="item trigger-group locked" data="add-server">Partner</div></a>
              </div>
             </div>
             <div class="messages-panel">
@@ -44,63 +58,77 @@
                 <div class="header">help</div>
                 <div class="messages">
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/220px-Cat03.jpg"/></div>
+                    <div class="icon"><img class="responsive-img" src="https://i.imgur.com/A256kGf.jpg"/></div>
                     <div class="body">
-                      <div class="user-name">your neighbors cat</div>
-                      <div class="content">Purr i heard this rumor where the humans are our owners, pfft, what do they know?! yet stick butt in face.</div>
+                      <div class="user-name">Adam</div>
+                      <div class="content">Hello</div>
                     </div>
                   </div>
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1105080326290845696/W7cDMv4s_200x200.jpg"/></div>
+                    <div class="icon"><img class="responsive-img" src="https://i.imgur.com/624tugY.jpg"/></div>
                     <div class="body">
-                      <div class="user-name">random dog </div>
-                      <div class="content">who are the people in our house?
+                      <div class="user-name">Eve</div>
+                      <div class="content">🍎 <i>*Eats apple*</i></div>
+                    </div>
+                  </div>
+                  <div class="message">
+                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1105210644406644736/lc7r2HOg_200x200.jpg"/></div>
+                    <div class="body">
+                      <div class="user-name">God</div>
+                      <div class="content">Are you fucking serious?
                       </div>
                     </div>
                   </div>
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
+                    <div class="icon"><img class="responsive-img ml-2 r180" style="fill:red;" src="https://cdn.discordapp.com/attachments/534174053184045057/554775908683350018/join.png"/></div>
                     <div class="body">
-                      <div class="user-name">audn</div>
-                      <div class="content">who's a good girl?<br> <img src="https://i.imgur.com/6nPVlC3.png" style="width:100px;"></div>
+                      <div class="content"><strong>God</strong> <span style="color:hsla(0,0%,100%,.6)">left the server</span></div>
                     </div>
                   </div>
-                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1105080326290845696/W7cDMv4s_200x200.jpg"/></div>
+
+                  <div class="message">
+                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
                     <div class="body">
-                      <div class="user-name">random dog </div>
-                      <div class="content">THATS A BALL! OH MY GOD OH MY GOD THIS IS THE BEST DAY OF MY LIFE
-                      </div>
+                      <div class="content"><strong>Envy</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
                     </div>
                   </div>
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/220px-Cat03.jpg"/></div>
+                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
                     <div class="body">
-                      <div class="user-name">your neighbors cat</div>
-                      <div class="content">it's just a ball</div>
+                      <div class="content"><strong>Greed</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
                     </div>
                   </div>
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
+                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
                     <div class="body">
-                      <div class="user-name">audn</div>
-                      <div class="content"><img src="https://i.imgur.com/5D2YoLc.png" style="width:100px;"></div>
+                      <div class="content"><strong>Gluttony</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
                     </div>
                   </div>
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/220px-Cat03.jpg"/></div>
+                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
                     <div class="body">
-                      <div class="user-name">your neighbors cat</div>
-                      <div class="content">you gonna throw that or what?</div>
+                      <div class="content"><strong>Lust</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
                     </div>
                   </div>
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
+                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
                     <div class="body">
-                      <div class="user-name">audn</div>
-                      <div class="content">I'm out of ideas. Send us your chat ideas <a href="#">here</a>!</div>
+                      <div class="content"><strong>Pride</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
                     </div>
                   </div>
+                  <div class="message">
+                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
+                    <div class="body">
+                      <div class="content"><strong>Sloth</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
+                    </div>
+                  </div>
+                  <div class="message">
+                    <div class="icon"><img class="responsive-img pl-2" src="https://discordapp.com/assets/b8029fe196b8f1382e90bbe81dab50dc.svg"/></div>
+                    <div class="body">
+                      <div class="content"><strong>Wrath</strong> <span style="color:hsla(0,0%,100%,.6)">joined the server</span></div>
+                    </div>
+                  </div>
+
 
 
                 </div>
@@ -129,6 +157,38 @@
                   </div>
                 </div>
               </div>
+              <div class="messages-group hide-unless-show" id="add-server">
+                <div class="header">Adding server</div>
+                <div class="messages">
+                  <div class="message">
+                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
+                    <div class="body">
+                      <div class="user-name">audn</div>
+                      <div class="content">Add your server on our website!
+                        <div class="body">
+                          <div class="embed-wrapper">
+                            <div class="color-pill"></div>
+                            <div class="content-inner">
+                              <div class="content">
+                                <div class="title">Discord.design</div>
+                                <p>Audun</p>
+                                <p>audunhilden@live.no</p>
+                                <p>Average 240 daily views</p><br/>
+                                <p><b>
+                                    <u>Quick contact  </u></b></p>
+                                <p>Avg. </p>
+                                <p>Avg. </p>
+                                <p>Avg. </p>
+                              </div><img class="thumb" src="https://i.imgur.com/0z3EQVg.png"/>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
