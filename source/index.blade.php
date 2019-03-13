e3877ea5539f9e12de721f8cac8ec5781f33b820
Audun <audunhilden@live.no>
2019-03-14 00:15:49 +0100

    added voting system

--- source/index.blade.php
+++ source/index.blade.php
@@ -23,8 +23,8 @@
               </div>
               <div class="items">
                 <div class="item"><a href="#"><img class="responsive-img" src="https://i.imgur.com/0z3EQVg.png"/></a></div>
-                <div class="item"><a href="https://apex-hub.com" target="_Blank"><img class="responsive-img" src="https://cdn.discordapp.com/icons/550004577404190729/04c1fac06656f28ad0ad5c3e8a2a6fea.png?"/></a></div>
                 <div class="item">
+                  <!--
                   <a href="#">
                     <button class="guild-add item trigger-group locked" data="add-server">
                       <span style="top: -4px;left: 2px;right: 0px; position: absolute;">
@@ -32,6 +32,7 @@
                       </span>
                     </button>
                   </a>
+                -->
                 </div>
               </div>
             </div>
@@ -45,12 +46,22 @@
                   <polygon fill-rule="nonzero" id="category-plus" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8">
                   </polygon>
                 </svg>
-               </div><a href="#">
-                  <div class="item trigger-group active" data="default">general</div></a><a href="#">
-                  <div class="item trigger-group" data="tips">tips</div></a><a href="#">
-                  <div class="item trigger-group locked" data="roadmap">roadmap</div></a>
+               </div>
+               <a href="#">
+                  <div class="item trigger-group active" data="default">general</div>
+                </a>
+                <a href="#">
+                  <div class="item trigger-group" data="tips">tips</div>
+                </a>
+                <a href="#">
+                  <div class="item trigger-group locked" data="roadmap">roadmap</div>
+                </a>
+                <a href="#">
+                  <div class="item trigger-group locked" data="help">help</div>
+                </a>
+                <a href="#">
                   <div style="display:none" class="item trigger-group locked" data="partner">Partner</div>
-                  </a>
+                </a>
              </div>
             </div>
             <div class="messages-panel">
@@ -102,7 +113,44 @@
                     <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
                     <div class="body">
                       <div class="user-name">audn</div>
-                      <div class="content">Look at the changes we've taken into concideration!</div>
+                      <div class="content">
+                        <strong class="discord-bold">
+                          Look at the changes we've taken into concideration!
+                        </strong>
+                        <br><p>- Press <span id="markdown">spacebar</span> to randomly select a theme.
+                          <div class="reactions">
+                            <div class="react">
+                              <img src="https://discordapp.com/assets/2af915882260fdb89538d1610e1d9baa.svg" alt="👍" draggable="false" class="emoji">
+                              <div class="reactionCount" style="min-width: 9px;">1</div>
+                            </div>
+                            <div class="react">
+                              <img src="https://discordapp.com/assets/9e1c3ddc9da7effefe8a370b7c33ed7b.svg" alt="👎" draggable="false" class="emoji">
+                              <div class="reactionCount" style="min-width: 9px;">1</div>
+                            </div>
+                          </div>
+                        <br><p>- Make it so people can actually chat online here.
+                          <div class="reactions">
+                            <div class="react">
+                              <img src="https://discordapp.com/assets/2af915882260fdb89538d1610e1d9baa.svg" alt="👍" draggable="false" class="emoji">
+                              <div class="reactionCount" style="min-width: 9px;">1</div>
+                            </div>
+                            <div class="react">
+                              <img src="https://discordapp.com/assets/9e1c3ddc9da7effefe8a370b7c33ed7b.svg" alt="👎" draggable="false" class="emoji">
+                              <div class="reactionCount" style="min-width: 9px;">1</div>
+                            </div>
+                          </div>
+                        <br><p>- Add some cool animations to buttons.
+                          <div class="reactions">
+                            <div class="react">
+                              <img src="https://discordapp.com/assets/2af915882260fdb89538d1610e1d9baa.svg" alt="👍" draggable="false" class="emoji">
+                              <div class="reactionCount" style="min-width: 9px;">1</div>
+                            </div>
+                            <div class="react">
+                              <img src="https://discordapp.com/assets/9e1c3ddc9da7effefe8a370b7c33ed7b.svg" alt="👎" draggable="false" class="emoji">
+                              <div class="reactionCount" style="min-width: 9px;">1</div>
+                            </div>
+                          </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -111,10 +159,29 @@
                 <div class="header">help</div>
                 <div class="messages">
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1088584545987891200/aaIAhnMm_400x400.jpg"/></div>
+                    <div class="icon"><img class="responsive-img" src="https://i.imgur.com/0z3EQVg.png"/></div>
                     <div class="body">
-                      <div class="user-name">audn</div>
-                      <div class="content">You can click channels to rename them!</div>
+                      <div class="user-name role-main">Discord.Style <span id="bot">BOT</span></div>
+                      <div class="content">
+                        <div class="body">
+                          <div class="embed-wrapper">
+                            <div class="color-pill-green"></div>
+                            <div class="content-inner">
+                              <div class="content">
+                                <div class="title">I'm here to help!</div>
+                                <p>This is a tab similar to FAQ (Frequently Asked Questions).</p>
+                                <br>
+                                <p>
+                                <strong class="disc-bold">How do I change template?</strong><br>
+                                You can <i>change servers</i> like you would normally do, and it changes the template. Keep in mind that this only works on the channels with a number as their server icon.
+                                <p><br>
+                                <strong class="disc-bold">(Not enough questions asked yet)</strong><br>
+                                You guys need to ask us some more questions, so that we can add them here!
+                              </div><img class="thumb" src="https://i.imgur.com/0z3EQVg.png"/>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
