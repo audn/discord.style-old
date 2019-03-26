9ab6d0fe5b4e95abe5b996a89bd9542a5b5ae072
Audun <audunhilden@live.no>
2019-03-26 12:48:05 +0100

    added menu

--- source/index.blade.php
+++ source/index.blade.php
@@ -23,6 +23,8 @@
               </div>
               <div class="items">
                 <div class="item"><a href="/"><img class="responsive-img" src="https://i.imgur.com/0z3EQVg.png"/></a></div>
+                <div class="item"><a href="/t1"><img class="responsive-img" src="https://i.imgur.com/k3Qmp2u.png"/></a></div>
+                <div class="item"><a href="/t2"><img class="responsive-img" src="https://i.imgur.com/TdFVnxI.png"/></a></div>
                 <div class="item">
                   <!--
                   <a href="#">
