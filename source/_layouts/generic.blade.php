1d295fe0daa1544a8616b76d683eb095ecc89ae9
Audun <audunhilden@live.no>
2019-03-26 03:32:42 +0100

    test

--- source/_layouts/generic.blade.php
+++ source/_layouts/generic.blade.php
@@ -10,23 +10,56 @@
 
 {{-- https://laravel.com/docs/5.4/blade#template-inheritance --}}
 @section('body')
-
-<div class="min-h-1/4-screen bg-black flex justify-center items-center py-4 bg-top bg-cover lazyload" data-bg="/img/legends.jpg">
-    <div class="p-8 mx-auto max-w-lg">
-        <img src="/img/logo-white.png" alt="Apex Hub" class="block mx-auto" width="300">
-    </div>
-</div>
-
-<article class="container mx-auto leading-normal">
-    <div class="flex p-4 justify-center">
-        <div class="flex-1 max-w-lg">
-            <h1 class="text-3xl font-normal lg:text-4xl mb-6 mt-4 leading-none{{ $page->permalink == '404.html' ? ' text-center' : '' }}">{{ $page->title }}</h1>
-
-            <div class="markdown-body">
-                @yield('content')
+<div class="container center-div">
+  <div class="row valign-wrapper no-valign-wrap-on-mobile">
+    <div class="col s12 l19">
+      <div class="discord-mockup z-depth-3">
+        <div class="items-panel">
+          <div class="actions">
+            <div class="close"></div>
+            <div class="minimize"></div>
+            <div class="zoom"></div>
+          </div>
+          <div class="items">
+            <div class="item"><a href="#"><img class="responsive-img" src="https://i.imgur.com/0z3EQVg.png"/></a></div>
+            <div class="item">
             </div>
+          </div>
         </div>
+        <div class="sidebar-panel hide-on-med-and-down">
+          <div class="header">Discord Templates</div>
+          <div class="items" id="items">
+            <div class="category" id="category">
+              <svg class="iconCollapsed-3hFp_8 iconTransition-2pOJ7l directionRight-O8AY4M" width="12" height="12" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 10L12 15 17 10"></path></svg>
+              @yield('content')
+              <svg style="float:right;" name="PlusAlt" class="addButtonIcon-3u-3Hu" width="18" height="18" viewBox="0 0 18 18">
+              <polygon fill-rule="nonzero" id="category-plus" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8">
+              </polygon>
+            </svg>
+           </div>
+           <a href="#">
+              <div class="item trigger-group active" data="default">general</div>
+            </a>
+            <a href="#">
+              <div class="item trigger-group locked" data="help">help</div>
+            </a>
+            <a href="#">
+              <div class="item trigger-group" data="roadmap">roadmap</div>
+            </a>
+            <a href="#">
+              <div class="item trigger-group" data="git">changes</div>
+            </a>
+            <a href="#">
+              <div style="display:none" class="item trigger-group locked" data="partner">Partner</div>
+            </a>
+
+         </div>
+        </div>
+
+        </div>
+      </div>
     </div>
-</article>
+  </div>
+</div>
 
 @endsection
