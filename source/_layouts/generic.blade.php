19e4725b167a7a08789c9714ad79d72cd3f3e9dc
Audun <audunhilden@live.no>
2019-03-26 03:35:40 +0100

    test again

--- source/_layouts/generic.blade.php
+++ source/_layouts/generic.blade.php
@@ -10,6 +10,24 @@
 
 {{-- https://laravel.com/docs/5.4/blade#template-inheritance --}}
 @section('body')
+
+<div class="min-h-1/4-screen bg-black flex justify-center items-center py-4 bg-top bg-cover lazyload" data-bg="/img/legends.jpg">
+    <div class="p-8 mx-auto max-w-lg">
+        <img src="/img/logo-white.png" alt="Apex Hub" class="block mx-auto" width="300">
+    </div>
+</div>
+
+<article class="container mx-auto leading-normal">
+    <div class="flex p-4 justify-center">
+        <div class="flex-1 max-w-lg">
+            <h1 class="text-3xl font-normal lg:text-4xl mb-6 mt-4 leading-none{{ $page->permalink == '404.html' ? ' text-center' : '' }}">{{ $page->title }}</h1>
+
+            <div class="markdown-body">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</article>
 <div class="container center-div">
   <div class="row valign-wrapper no-valign-wrap-on-mobile">
     <div class="col s12 l19">
@@ -31,20 +49,20 @@
           <div class="items" id="items">
             <div class="category" id="category">
               <svg class="iconCollapsed-3hFp_8 iconTransition-2pOJ7l directionRight-O8AY4M" width="12" height="12" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 10L12 15 17 10"></path></svg>
-              @yield('content')
+              Important things
               <svg style="float:right;" name="PlusAlt" class="addButtonIcon-3u-3Hu" width="18" height="18" viewBox="0 0 18 18">
               <polygon fill-rule="nonzero" id="category-plus" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8">
               </polygon>
             </svg>
            </div>
            <a href="#">
-              <div class="item trigger-group active" data="default">general</div>
+              <div class="item trigger-group active" data="default">{{ $page->channel1 }}</div>
             </a>
             <a href="#">
-              <div class="item trigger-group locked" data="help">help</div>
+              <div class="item trigger-group locked" data="help">{{ $t1->channel1 }}</div>
             </a>
             <a href="#">
-              <div class="item trigger-group" data="roadmap">roadmap</div>
+              <div class="item trigger-group" data="roadmap">{{ $page->$channel1 }}</div>
             </a>
             <a href="#">
               <div class="item trigger-group" data="git">changes</div>
@@ -55,6 +73,28 @@
 
          </div>
         </div>
+        <div class="messages-panel">
+          <div class="messages-group hide-unless-show" id="default">
+            <div class="header">general</div>
+            <div class="messages">
+
+              <div class="message">
+                  <div class="icon pfp column">
+                    <img class="responsive-img" src="https://pbs.twimg.com/profile_images/1106214503069429763/Y8VhNXcd_400x400.png"/>
+                  </div>
+                <div class="body">
+                  <div class="msg column">
+                  <div class="user-name">audn <span class="timestamp">Today at 13:37</span></div>
+
+                    @yield('content')
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            </div>
+          </div>
 
         </div>
       </div>
