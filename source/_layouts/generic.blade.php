5a8de4e534a67fce47eb5d7f56497b159e9207e5
Audun <audunhilden@live.no>
2019-03-26 03:42:52 +0100

    removed active

--- source/_layouts/generic.blade.php
+++ source/_layouts/generic.blade.php
@@ -11,23 +11,6 @@
 {{-- https://laravel.com/docs/5.4/blade#template-inheritance --}}
 @section('body')
 
-<div class="min-h-1/4-screen bg-black flex justify-center items-center py-4 bg-top bg-cover lazyload" data-bg="/img/legends.jpg">
-    <div class="p-8 mx-auto max-w-lg">
-        <img src="/img/logo-white.png" alt="Apex Hub" class="block mx-auto" width="300">
-    </div>
-</div>
-
-<article class="container mx-auto leading-normal">
-    <div class="flex p-4 justify-center">
-        <div class="flex-1 max-w-lg">
-            <h1 class="text-3xl font-normal lg:text-4xl mb-6 mt-4 leading-none{{ $page->permalink == '404.html' ? ' text-center' : '' }}">{{ $page->title }}</h1>
-
-            <div class="markdown-body">
-                @yield('content')
-            </div>
-        </div>
-    </div>
-</article>
 <div class="container center-div">
   <div class="row valign-wrapper no-valign-wrap-on-mobile">
     <div class="col s12 l19">
@@ -57,10 +40,10 @@
            </div>
            @foreach ($page->channels as $channel)
            <a href="#">
-              <div class="item trigger-group active" data="default">{{ $channels }}</div>
+              <div class="item trigger-group" data="default">{{ $channels }}</div>
             </a>
             @endforeach
-            
+
            <a href="#">
               <div class="item trigger-group active" data="default">{{ $page->channel1 }}</div>
             </a>
