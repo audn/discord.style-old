1149fff087e71ef1b0204b11e2bd972dddffa09e
Audun <audunhilden@live.no>
2019-03-29 04:44:01 +0100

    fixed title

--- source/_layouts/generic.blade.php
+++ source/_layouts/generic.blade.php
@@ -5,7 +5,7 @@
     {{-- You can use the head stack to push page specific elements to the head, such as stylesheets --}}
 @endpush
 
-@section('title', $page->title . ' | Apex Hub')
+@section('title', $page->title . ' | discord.style')
 @section('description', $page->description)
 
 {{-- https://laravel.com/docs/5.4/blade#template-inheritance --}}
@@ -31,7 +31,7 @@
 
            <div class="category" id="category">
              <svg class="iconCollapsed-3hFp_8 iconTransition-2pOJ7l directionRight-O8AY4M" width="12" height="12" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 10L12 15 17 10"></path></svg>
-              {{ $page->category1 }} 
+              {{ $page->category1 }}
              <svg style="float:right;" name="PlusAlt" class="addButtonIcon-3u-3Hu" width="18" height="18" viewBox="0 0 18 18">
              <polygon fill-rule="nonzero" id="category-plus" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8"></polygon>
            </svg>
