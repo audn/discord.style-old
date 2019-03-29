b1b79bc0ca9301c4f20d7a2a6c68389479dc6b54
Audun <audunhilden@live.no>
2019-03-29 04:29:49 +0100

    fixed tons

--- source/index.blade.php
+++ source/index.blade.php
@@ -28,7 +28,7 @@
               <div class="items" id="items">
                 <div class="category" id="category">
                   <svg class="iconCollapsed-3hFp_8 iconTransition-2pOJ7l directionRight-O8AY4M" width="12" height="12" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 10L12 15 17 10"></path></svg>
-                  Important things
+                  <img src="https://discordapp.com/assets/e4d52f4d69d7bba67e5fd70ffe26b70d.svg" class="emoji jumboable" alt=":star:" draggable="false"> Text Channels
                   <svg style="float:right;" name="PlusAlt" class="addButtonIcon-3u-3Hu" width="18" height="18" viewBox="0 0 18 18">
                   <polygon fill-rule="nonzero" id="category-plus" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8">
                   </polygon>
@@ -38,8 +38,22 @@
                   <div class="item trigger-group active" data="default">general</div>
                 </a>
                 <a href="#">
-                  <div class="item trigger-group locked" data="help">help</div>
+                  <div class="item trigger-group locked" data="streamer">streamer</div>
                 </a>
+                <a href="#">
+                  <div class="item trigger-group locked" data="community">community</div>
+                </a>
+                <a href="#">
+                  <div class="item trigger-group locked mb-6" data="startup">startup</div>
+                </a>
+                <div class="category" id="category">
+                  <svg class="iconCollapsed-3hFp_8 iconTransition-2pOJ7l directionRight-O8AY4M" width="12" height="12" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 10L12 15 17 10"></path></svg>
+                  <img src="https://discordapp.com/assets/d0df7bf4acd843defa4e417cf767a574.svg" class="emoji jumboable" alt=":sunglasses:" draggable="false"> Website things
+                  <svg style="float:right;" name="PlusAlt" class="addButtonIcon-3u-3Hu" width="18" height="18" viewBox="0 0 18 18">
+                  <polygon fill-rule="nonzero" id="category-plus" class="category-expand" fill="#6d6e71" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8">
+                  </polygon>
+                </svg>
+               </div>
                 <a href="#">
                   <div class="item trigger-group" data="roadmap">roadmap</div>
                 </a>
@@ -66,14 +80,34 @@
                       <div class="user-name">audn <span class="timestamp">Today at 13:37</span></div>
                       <div class="content">
                         <strong class="disc-bold">Welcome to our website!</strong>
-                        <br>You can get started by clicking the server icons on the left.
-                        <br>The numbers indicate what kind of template it is (1st template, 2nd template).. and so on.
-                        <p><br>
-                        <strong class="disc-bold">Help!</strong>
-                        <br>
-                          We're always online to help! But it's probably best for you to check out
-                        <a href="#">
-                          <span class="channelID trigger-group" data="help">#help</span></a> first!
+                        <p>
+                        <div class="wrapper-35wsBm userSelectNone-Iy6XEP cursorDefault-331ZcI">
+                          <h5 class="h5-18_1nd title-3sZWYQ size12-3R0845 height16-2Lv3qA weightSemiBold-NJexzi header-2BTCnc marginBottom8-AtZOdT">
+                            You've been invited to join a server
+                          </h5>
+                          <div class="content-2U5lSY">
+                            <div class="icon-3o6xvg guildIconImage-3qTk45 guildIcon-lQ0uiM iconSizeLarge-161qtT iconActiveLarge-2nzn9z" style="background-image: url(&quot;https://i.imgur.com/0z3EQVg.png?&quot;);">
+                            </div>
+                            <div class="flex-1xMQg5 flex-1O1GKY vertical-V37hAW flex-1O1GKY directionColumn-35P_nr justifyCenter-3D2jYp alignStretch-DpGPf3 noWrap-3jynv6 guildInfo-1STtYi" style="flex: 1 1 auto;">
+                            <div class="guildName-2hvnt_ marginBottom4-2qk4Hy medium-zmzTW- size16-14cGz5 height20-mO2eIN weightSemiBold-NJexzi">
+                              discord.style
+                            </div>
+                            <div class="guildDetail-1nRKNE small-29zrCQ size12-3R0845 height16-2Lv3qA weightSemiBold-NJexzi">
+                          <div class="flex-1xMQg5 flex-1O1GKY horizontal-1ae9ci horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG justifyStart-2NDFzi alignCenter-1dQNNs noWrap-3jynv6" style="flex: 0 1 auto;">
+                            <span class="statusOnline-8PnF5L status-2L8Zc7">&nbsp;</span>
+                            <span class="onlineCount-w6_WmG">10,521 Online</span>
+                            <span class="statusOffline-37RKt7 status-2L8Zc7">&nbsp;</span>
+                            <span>52,314 Members</span>
+                          </div>
+                        </div>
+                      </div>
+                      <button type="button" class="button-3To2tQ height20-mO2eIN button-38aScr lookFilled-1Gx00P colorGreen-29iAKY buttonSize-DbrWhv grow-q77ONN">
+                      <div class="contents-18-Yxp buttonInner-1IRxqd">
+                        <div class="buttonContent-18Mz6_">Join</div>
+                      </div>
+                    </button>
+                  </div>
+                </div>
 
                       </div>
                     </div>
@@ -147,31 +181,49 @@
                   </div>
                 </div>
               </div>
-              <div class="messages-group hide-unless-show" id="help">
-                <div class="header">help</div>
+              <div class="messages-group hide-unless-show" id="streamer">
+                <div class="header">streamer</div>
                 <div class="messages">
                   <div class="message">
-                    <div class="icon"><img class="responsive-img" src="https://i.imgur.com/0z3EQVg.png"/></div>
+                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1106214503069429763/Y8VhNXcd_400x400.png"/></div>
                     <div class="body">
-                      <div class="user-name role-main">Discord.Style <span id="bot">BOT</span></div>
+                      <div class="user-name">audn</div>
                       <div class="content">
                         <div class="body">
-                          <div class="embed-wrapper">
-                            <div class="color-pill-green"></div>
-                            <div class="content-inner">
-                              <div class="content">
-                                <div class="title">I'm here to help!</div>
-                                <p>This is a tab similar to FAQ (Frequently Asked Questions).</p>
-                                <br>
-                                <p>
-                                <strong class="disc-bold">How do I change template?</strong><br>
-                                You can <i>change servers</i> like you would normally do, and it changes the template. Keep in mind that this only works on the channels with a number as their server icon.
-                                <p><br>
-                                <strong class="disc-bold">(Not enough questions asked yet)</strong><br>
-                                You guys need to ask us some more questions, so that we can add them here!
-                              </div><img class="thumb" src="https://i.imgur.com/0z3EQVg.png"/>
-                            </div>
-                          </div>
+                          <strong>Streamer templates!</strong><br>
+                          Template for Content Creators, such as Streamers or Youtubers.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="messages-group hide-unless-show" id="community">
+                <div class="header">streamer</div>
+                <div class="messages">
+                  <div class="message">
+                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1106214503069429763/Y8VhNXcd_400x400.png"/></div>
+                    <div class="body">
+                      <div class="user-name">audn</div>
+                      <div class="content">
+                        <div class="body">
+                          <strong>Community templates!</strong><br>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="messages-group hide-unless-show" id="startup">
+                <div class="header">streamer</div>
+                <div class="messages">
+                  <div class="message">
+                    <div class="icon"><img class="responsive-img" src="https://pbs.twimg.com/profile_images/1106214503069429763/Y8VhNXcd_400x400.png"/></div>
+                    <div class="body">
+                      <div class="user-name">audn</div>
+                      <div class="content">
+                        <div class="body">
+                          <strong>Startup templates!</strong><br>
                         </div>
                       </div>
                     </div>
