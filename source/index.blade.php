be4f47104c16bf75fe1b6e9d60eaed3c39fbda57
Audun <audunhilden@live.no>
2019-03-30 02:11:34 +0100

    did some wierd stuff to the javascript

--- source/index.blade.php
+++ source/index.blade.php
@@ -102,7 +102,9 @@
                             <div class="guildDetail-1nRKNE small-29zrCQ size12-3R0845 height16-2Lv3qA weightSemiBold-NJexzi">
                             <div class="flex-1xMQg5 flex-1O1GKY horizontal-1ae9ci horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG justifyStart-2NDFzi alignCenter-1dQNNs noWrap-3jynv6" style="flex: 0 1 auto;">
                             <span class="statusOnline-8PnF5L status-2L8Zc7">&nbsp;</span>
-                            <span class="onlineCount-w6_WmG">10,521 Online</span>
+                            <span class="onlineCount-w6_WmG">
+
+                            </span>
                             <span class="statusOffline-37RKt7 status-2L8Zc7">&nbsp;</span>
                             <span>52,314 Members</span>
                           </div>
@@ -122,6 +124,7 @@
                           });
                           discordWidget.render();
                       </script>
+                      <div class="discord-widget"></div>
                       <div class="discord-widget"></div>
                         </div>
                       </div>
